6d79234ff5fc89a5dd7d2f9c3efddbc4733049eb
Your Name <your_email@example.com>
2026-06-12 01:35:57 +0800

    Show all env variable variations

--- backend/src/main/java/com/practice/controller/HealthController.java
+++ backend/src/main/java/com/practice/controller/HealthController.java
@@ -26,13 +26,20 @@ public class HealthController {
         Map<String, String> env = new HashMap<>();
         env.put("PORT", System.getenv("PORT"));
         env.put("MYSQLHOST", System.getenv("MYSQLHOST"));
+        env.put("MYSQL_PORT", System.getenv("MYSQL_PORT"));
         env.put("MYSQLPORT", System.getenv("MYSQLPORT"));
         env.put("MYSQLDATABASE", System.getenv("MYSQLDATABASE"));
+        env.put("MYSQL_DATABASE", System.getenv("MYSQL_DATABASE"));
         env.put("MYSQLUSER", System.getenv("MYSQLUSER"));
+        env.put("MYSQL_USER", System.getenv("MYSQL_USER"));
         env.put("MYSQLPASSWORD", System.getenv("MYSQLPASSWORD"));
+        env.put("MYSQL_PASSWORD", System.getenv("MYSQL_PASSWORD"));
         env.put("REDISHOST", System.getenv("REDISHOST"));
+        env.put("REDIS_HOST", System.getenv("REDIS_HOST"));
         env.put("REDISPORT", System.getenv("REDISPORT"));
+        env.put("REDIS_PORT", System.getenv("REDIS_PORT"));
         env.put("REDISPASSWORD", System.getenv("REDISPASSWORD"));
+        env.put("REDIS_PASSWORD", System.getenv("REDIS_PASSWORD"));
         return Result.success(env);
     }
 }
